589a38aa521288dccc9503bf3334ab9ae75b6b76
naturefitness.cmc@gmail.com <Daylacaipass@25>
2022-09-13 19:36:26 +0900

    fix tablet image url

--- pages.groovy
+++ pages.groovy
@@ -1,9 +1,9 @@
-'100.md'(title: 'ログイン機能（Login', numbering: true, documentTocDepth: 1, pageTocDepth:2)
-'101.md'(title: '会員登録（Member registration）', numbering: true, documentTocDepth: 1, pageTocDepth:2)
-'102.md'(title: 'ホーム＆メニュー（Home Page)', numbering: true, documentTocDepth: 1, pageTocDepth:2)
-'200.md'(title: '[TB]ログイン', numbering: true, documentTocDepth: 1, pageTocDepth:2)
-'201.md'(title: '[TB]マシン設定', numbering: true, documentTocDepth: 1, pageTocDepth:2)
+// Mobile app
+"100.md"(title: "ログイン機能（Login", numbering: true, documentTocDepth: 1, pageTocDepth:2)
+"101.md"(title: "会員登録（Member registration）", numbering: true, documentTocDepth: 1, pageTocDepth:2)
+"102.md"(title: "ホーム＆メニュー（Home Page)", numbering: true, documentTocDepth: 1, pageTocDepth:2)
+// Tablet app
+"200.md"(title: "ログイン", numbering: true, documentTocDepth: 1, pageTocDepth:2)
+"201.md"(title: "マシン設定", numbering: true, documentTocDepth: 1, pageTocDepth:2)
 
-
-
-'999.md'(title: 'ホーム＆メニュー（Template)', numbering: false, documentTocDepth: 1, pageTocDepth:3)
+"999.md"(title: "ホーム＆メニュー（Template)", numbering: false, documentTocDepth: 1, pageTocDepth:3)
