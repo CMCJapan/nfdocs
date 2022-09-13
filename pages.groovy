bcb83ea4092ac15c9c9ab8f6a788b381bd5cd249
naturefitness.cmc@gmail.com <Daylacaipass@25>
2022-09-13 17:41:20 +0900

    exitsted docs

---
+++ pages.groovy
@@ -0,0 +1,9 @@
+'100.md'(title: 'ログイン機能（Login', numbering: true, documentTocDepth: 1, pageTocDepth:2)
+'101.md'(title: '会員登録（Member registration）', numbering: true, documentTocDepth: 1, pageTocDepth:2)
+'102.md'(title: 'ホーム＆メニュー（Home Page)', numbering: true, documentTocDepth: 1, pageTocDepth:2)
+'200.md'(title: 'Talbet login', numbering: true, documentTocDepth: 1, pageTocDepth:2)
+
+
+
+
+'999.md'(title: 'ホーム＆メニュー（Template)', numbering: false, documentTocDepth: 1, pageTocDepth:3)
