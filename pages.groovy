2e92d0234caa180c064f7410039593ec2627c5fc
pdhiepCMCJapan <pdhiep@cmc-japan.co.jp>
2022-09-13 18:43:40 +0900

    tablet doc login & setting machine

--- pages.groovy
+++ pages.groovy
@@ -1,8 +1,8 @@
 '100.md'(title: 'ログイン機能（Login', numbering: true, documentTocDepth: 1, pageTocDepth:2)
 '101.md'(title: '会員登録（Member registration）', numbering: true, documentTocDepth: 1, pageTocDepth:2)
 '102.md'(title: 'ホーム＆メニュー（Home Page)', numbering: true, documentTocDepth: 1, pageTocDepth:2)
-'200.md'(title: 'Talbet login', numbering: true, documentTocDepth: 1, pageTocDepth:2)
-
+'200.md'(title: '[TB]ログイン', numbering: true, documentTocDepth: 1, pageTocDepth:2)
+'201.md'(title: '[TB]マシン設定', numbering: true, documentTocDepth: 1, pageTocDepth:2)
 
 
 
