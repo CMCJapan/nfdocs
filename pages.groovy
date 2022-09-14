d4f850542491530d6b4683e5e9843d8a635af4c9
naturefitness.cmc@gmail.com <Daylacaipass@25>
2022-09-14 14:31:08 +0900

    build tablet

--- pages.groovy
+++ pages.groovy
@@ -5,5 +5,6 @@
 // Tablet app
 "200.md"(title: "ログイン", numbering: true, documentTocDepth: 1, pageTocDepth:2)
 "201.md"(title: "マシン設定", numbering: true, documentTocDepth: 1, pageTocDepth:2)
+"202.md"(title: "Kioskモードオペレーション", numbering: true, documentTocDepth: 1, pageTocDepth:2)
 
 "999.md"(title: "ホーム＆メニュー（Template)", numbering: false, documentTocDepth: 1, pageTocDepth:3)
