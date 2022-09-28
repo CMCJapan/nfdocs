b5b4d8276ec8169143089111a5bddae6505d7bd5
pdhiepCMCJapan <pdhiep@cmc-japan.co.jp>
2022-09-28 15:45:27 +0900

    gym layout cònfig

--- pages.groovy
+++ pages.groovy
@@ -2,6 +2,7 @@
 "100.md"(title: "ログイン機能（Login", numbering: true, documentTocDepth: 1, pageTocDepth:2)
 "101.md"(title: "会員登録（Member registration）", numbering: true, documentTocDepth: 1, pageTocDepth:2)
 "102.md"(title: "ホーム＆メニュー（Home Page)", numbering: true, documentTocDepth: 1, pageTocDepth:2)
+"103-jp.md"(title: "ジム詳細", numbering: true, documentTocDepth: 1, pageTocDepth:2)
 // Tablet app
 "200.md"(title: "ログイン", numbering: true, documentTocDepth: 1, pageTocDepth:2)
 "201.md"(title: "マシン設定", numbering: true, documentTocDepth: 1, pageTocDepth:2)
