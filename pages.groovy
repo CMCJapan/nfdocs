626a69923a1829776210ebabb53b41c139e31097
pdhiepCMCJapan <pdhiep@cmc-japan.co.jp>
2022-10-12 14:46:22 +0900

    update layout tablet

--- pages.groovy
+++ pages.groovy
@@ -8,5 +8,6 @@
 "200.md"(title: "ログイン", numbering: true, documentTocDepth: 1, pageTocDepth:2)
 "201.md"(title: "マシン設定", numbering: true, documentTocDepth: 1, pageTocDepth:2)
 "202.md"(title: "Kioskモードオペレーション", numbering: true, documentTocDepth: 1, pageTocDepth:2)
+"299-jp.md"(title: "サイド機能", numbering: true, documentTocDepth: 1, pageTocDepth:2)
 
 "999.md"(title: "ホーム＆メニュー（Template)", numbering: false, documentTocDepth: 1, pageTocDepth:3)
