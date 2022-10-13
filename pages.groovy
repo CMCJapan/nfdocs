53d1225e4dae1b3057f67bdfa1a47961e8261365
pdhiepCMCJapan <pdhiep@cmc-japan.co.jp>
2022-10-13 15:28:56 +0900

    add online video

--- pages.groovy
+++ pages.groovy
@@ -4,6 +4,8 @@
 "102.md"(title: "ホーム＆メニュー（Home Page)", numbering: true, documentTocDepth: 1, pageTocDepth:2)
 "103-jp.md"(title: "ジム詳細", numbering: true, documentTocDepth: 1, pageTocDepth:2)
 "104-jp.md"(title: "チェックイン", numbering: true, documentTocDepth: 1, pageTocDepth:2)
+"106-jp.md"(title: "オラインビデオ", numbering: true, documentTocDepth: 1, pageTocDepth:2)
+
 // Tablet app
 "200.md"(title: "ログイン", numbering: true, documentTocDepth: 1, pageTocDepth:2)
 "201.md"(title: "マシン設定", numbering: true, documentTocDepth: 1, pageTocDepth:2)
