801702fdfb87374a38ff31b4f12b2d19a0156734
pdhiepCMCJapan <pdhiep@cmc-japan.co.jp>
2022-10-20 11:17:02 +0900

    delete acc 6 update home

--- pages.groovy
+++ pages.groovy
@@ -1,11 +1,13 @@
 // Mobile app
 "100.md"(title: "ログイン機能（Login", numbering: true, documentTocDepth: 1, pageTocDepth:2)
 "101.md"(title: "会員登録（Member registration）", numbering: true, documentTocDepth: 1, pageTocDepth:2)
-"102.md"(title: "ホーム＆メニュー（Home Page)", numbering: true, documentTocDepth: 1, pageTocDepth:2)
-"103-jp.md"(title: "ジム詳細", numbering: true, documentTocDepth: 1, pageTocDepth:2)
-"104-jp.md"(title: "チェックイン", numbering: true, documentTocDepth: 1, pageTocDepth:2)
-"105-jp.md"(title: "本日のトレーニングメーニュー", numbering: true, documentTocDepth: 1, pageTocDepth:2)
-"106-jp.md"(title: "オラインビデオ", numbering: true, documentTocDepth: 1, pageTocDepth:2)
+"103-jp.md"(title: "退会とアカウント削除（Unsubscribe & account deletion)", numbering: true, documentTocDepth: 1, pageTocDepth:2)
+"105-jp.md"(title: "ホーム＆メニュー（Home & Menu)", numbering: true, documentTocDepth: 1, pageTocDepth:2)
+"106-jp.md"(title: "ジム詳細 (Gym Information)", numbering: true, documentTocDepth: 1, pageTocDepth:2)
+"107-jp.md"(title: "チェックイン (Check-in)", numbering: true, documentTocDepth: 1, pageTocDepth:2)
+"108-jp.md"(title: "本日のトレーニングメーニュー (Training menu)", numbering: true, documentTocDepth: 1, pageTocDepth:2)
+"109-jp.md"(title: "オラインビデオ (Online video)", numbering: true, documentTocDepth: 1, pageTocDepth:2)
+"199-jp.md"(title: "サイド機能 (Side function)", numbering: true, documentTocDepth: 1, pageTocDepth:2)
 
 // Tablet app
 "200.md"(title: "ログイン", numbering: true, documentTocDepth: 1, pageTocDepth:2)
